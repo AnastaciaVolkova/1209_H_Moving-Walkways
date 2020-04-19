90bf2cb85b2118fa0ec7df62d883aace33f1cd6d
Anastacia Volkova <polarbolide@gmail.com>
2020-04-19 22:59:43 +0200

    refactor: Change stof to stoi

--- src/main_ut.cpp
+++ src/main_ut.cpp
@@ -36,7 +36,7 @@ public:
         getline(in_stream, x);
         istringstream iss(x);
         vector<string> x2(istream_iterator<string>{iss}, istream_iterator<string>());
-        in_data.walk_ways_number = stof(x2[0]);
+        in_data.walk_ways_number = stoi(x2[0]);
         in_data.distance_to_walk= stof(x2[1]);
 
         while(getline(in_stream, x)&&(x != "")){
