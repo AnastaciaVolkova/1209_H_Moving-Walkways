a319a94cb2ecfbce05aa59298a2edaf8d2e97236
Anastacia Volkova <polarbolide@gmail.com>
2020-04-21 22:36:05 +0200

    test: Add test generator

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import numpy as np
+
+d_range = 40
 
 # Limak goes with speed 1.
 # Energy is neigther accumulated, nor consumed.
@@ -19,7 +22,7 @@ def AcceleratedWalk(l1, s1, l2, s2, d1, d2):
 
     t1 = l1/(s1+1+d1)
 
-    energy = t1 * d1
+    energy = -t1 * d1
 
     if d2 == 0:
         t2 = l2 / (s2 + 1)
@@ -28,11 +31,11 @@ def AcceleratedWalk(l1, s1, l2, s2, d1, d2):
 
         l21 = t21 * (s2 + 1 + d2)
 
-        if l21 > l:
-            t21 = l/(s2+1+d2)
+        if l21 > l2:
+            t21 = l2/(s2+1+d2)
             t22 = 0
         else:
-            l22 = l - l21
+            l22 = l2 - l21
             t22 = l22/(s2+1)
         t2 = t21 + t22
 
@@ -40,13 +43,46 @@ def AcceleratedWalk(l1, s1, l2, s2, d1, d2):
     return t
 
 
+# Test cases:
+# The values of lengths and speeds much bigger or equal to 1
+def CreateTestCases():
+    num_cases = 100
+    test_cases = np.empty((num_cases, 16, 4))
+    func_bits = (lambda: 10*np.random.rand(num_cases),
+                 lambda: 100 + 100*np.random.rand(num_cases))
+    for i in range(16):
+        bits = list("{:04b}".format(i))
+        bits = [int(b) for b in bits]
+        tc = np.array([func_bits[bits[b]]() for b in range(4)])
+        tc = tc.transpose()
+        tc = tc.reshape((num_cases, 1, 4))
+        test_cases[:, i:i+1, :] = tc
+    return test_cases
+
 def main():
-    l1 = int(sys.argv[1])
-    s1 = int(sys.argv[2])
-    l2 = int(sys.argv[3])
-    s2 = int(sys.argv[4])
-    print(NormalWalk(l1, s1, l2, s2))
-    print(AcceleratedWalk(l1, s1, l2, s2, 0, 0))
+    if len(sys.argv) == 5:
+        l1 = int(sys.argv[1])
+        s1 = int(sys.argv[2])
+        l2 = int(sys.argv[3])
+        s2 = int(sys.argv[4])
+    else:
+        test_cases = CreateTestCases()
+        for tc_row in test_cases:
+            for tc in tc_row:
+                l1, s1, l2, s2 = tc
+                tn = NormalWalk(l1, s1, l2, s2)
+                tmin = tn
+                usefull_case = None
+                for di in range(0, d_range+1):
+                    d1 = -di / d_range
+                    for dj in range(0, d_range):
+                        d2 = dj / d_range
+                        t = AcceleratedWalk(l1, s1, l2, s2, d1, d2)
+                        if tmin-t>1e-6:
+                            usefull_case = tc.tolist() + [d1, d2, tmin, t]
+                            tmin = t
+                if usefull_case is not None:
+                    print(usefull_case)
 
 
 if __name__ == "__main__":
