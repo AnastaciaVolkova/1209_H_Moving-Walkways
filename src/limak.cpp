8e4c1e706d3d6182f84d2f5fa3de22e6a8061b9c
Anastacia Volkova <polarbolide@gmail.com>
2020-04-30 00:21:48 +0200

    feat: Compare double with double

--- src/limak.cpp
+++ src/limak.cpp
@@ -1,7 +1,7 @@
 // definition for Limak (walker).
 #include "limak.hpp"
 
-Limak::Limak() :speed_(0), energy_(0) {};
+Limak::Limak() :speed_(0.0), energy_(0.0) {};
 
 // Speed getter.
 double Limak::GetSpeed() { return speed_; };
@@ -18,24 +18,21 @@ double Limak::GetEnergy() { return energy_; };
 // returns how many seconds Limak walks.
 double Limak::MakeWalk(const Walkway& walkway) {
     // How much energy Limak accumulates/spends per second.
-    double d_e = (1 - speed_);
+    double d_e = (1.0 - speed_);
 
     // How much time Limak needs to walk.
     double dt = walkway.GetLength() / (speed_ + walkway.GetSpeed());
 
     energy_ += (dt * d_e);
 
-    //if ((energy_ < 0) && (abs(energy_)>1e-19)/* && (walkway.GetSpeed() == 0)*/)
-    //    throw "Out of energy";
-
-    if ((energy_ < 0) && (abs(energy_) > 1e-19)) {
+    if (energy_ < 0.0) {
         energy_ -= (dt * d_e); // Return back wasted energy.
         double t1 = energy_ / (speed_ - 1);
         // Distance to walk with current speed.
         double l1 = t1 * (speed_ + walkway.GetSpeed());
         // Distance to walk with decreased speed.
         double l2 = walkway.GetLength() - l1;
-        if (l2 < 0)
+        if (l2 < 0.0)
             throw "Negative distance to walk";
         SetSpeed(1);
         double t2 = l2 / (walkway.GetSpeed() + GetSpeed());
